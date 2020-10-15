96c4141625e7e6633336e090107e0747fb5153e2
Nimaoth <nicolas.otremba@web.de>
2020-10-15 21:52:05 +0200

    bug fixes, improved rendering of long lines

--- build.ps1
+++ build.ps1
@@ -1,4 +1,5 @@
 [CmdletBinding()]
 param ([Parameter(ValueFromRemainingArguments)] [string[]] $Passthrough)
-Copy-Item "../libraries/GLFW/glfw/lib/glfw3.dll" "./bin"
-&cheezc ./src/main.che --out ./bin --name chim --time --error-source --modules ../libraries/GLFW/glfw ../libraries/imgui/src --print-ast-analysed ./bin/int/ast.chea @Passthrough
+Copy-Item "../libraries/GLFW/glfw/lib/glfw3.dll" "./bin" -Force
+Copy-Item "./fonts" "./bin" -Recurse -Force
+&cheezc ./src/main.che --out ./bin --name chim --time --error-source --modules ../libraries/GLFW/glfw ../libraries/imgui/src --subsystem console --print-ast-analysed ./bin/int/ast.chea @Passthrough
