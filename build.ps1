8dcf671c035a54f1a84c21d406345dd36394f6e1
Nimaoth <nicolas.otremba@web.de>
2020-10-16 15:57:19 +0200

    added tab rendering, use ArenaAllocator for buffer

--- build.ps1
+++ build.ps1
@@ -2,4 +2,4 @@
 param ([Parameter(ValueFromRemainingArguments)] [string[]] $Passthrough)
 Copy-Item "../libraries/GLFW/glfw/lib/glfw3.dll" "./bin" -Force
 Copy-Item "./fonts" "./bin" -Recurse -Force
-&cheezc ./src/main.che --out ./bin --name chim --time --error-source --modules ../libraries/GLFW/glfw ../libraries/imgui/src --subsystem console --print-ast-analysed ./bin/int/ast.chea @Passthrough
+&cheezc ./src/main.che --out ./bin --name chim --time --error-source --modules ../libraries/GLFW/glfw ../libraries/imgui/src --subsystem console --opt --print-ast-analysed ./bin/int/ast.chea @Passthrough
