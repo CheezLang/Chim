1b20ddcf0a613db539ae66fb99aace34cd563cd6
Nimaoth <nicolas.otremba@web.de>
2020-10-23 23:30:48 +0200

    set icon for window at runtime

--- build.ps1
+++ build.ps1
@@ -2,4 +2,5 @@
 param ([Parameter(ValueFromRemainingArguments)] [string[]] $Passthrough)
 Copy-Item "../libraries/GLFW/glfw/lib/glfw3.dll" "./bin" -Force
 Copy-Item "./fonts" "./bin" -Recurse -Force
-&cheezc ./src/main.che --trace-stack --out ./bin --name chim --time --error-source --modules ../libraries/GLFW/glfw ../libraries/imgui/src --subsystem console --print-ast-analysed ./bin/int/ast.chea --print-linker-args @Passthrough
+Copy-Item "./icons/icon.ico" "./bin" -Force
+&cheezc ./src/main.che --trace-stack --out ./bin --name chim --time --error-source --modules ../libraries/GLFW/glfw ../libraries/stb_image/stb_image ../libraries/imgui/src --subsystem console --print-ast-analysed ./bin/int/ast.chea --print-linker-args @Passthrough
