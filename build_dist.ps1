e514f04e75280fce54ae2fc1148e54b7c85c0be5
Nimaoth <nicolas.otremba@web.de>
2020-10-23 23:33:34 +0200

    fixed build_dist.ps1

--- build_dist.ps1
+++ build_dist.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param ([Parameter(ValueFromRemainingArguments)] [string[]] $Passthrough)
-&cheezc ./src/main.che --out ./bin --name chim --time --error-source --modules ../libraries/GLFW/glfw ../libraries/imgui/src --subsystem windows --opt @Passthrough
+&cheezc ./src/main.che --out ./bin --name chim --time --error-source --modules ../libraries/GLFW/glfw ../libraries/imgui/src ../libraries/stb_image/stb_image --subsystem windows --opt @Passthrough
 
 $install_dir = "D:\Program Files\chim"
 if (-not (Test-Path $install_dir)) {
@@ -9,4 +9,5 @@ if (-not (Test-Path $install_dir)) {
 
 Copy-Item "../libraries/GLFW/glfw/lib/glfw3.dll" $install_dir -Force
 Copy-Item "./fonts" $install_dir -Recurse -Force
+Copy-Item "./icons/icon.ico" $install_dir -Force
 Copy-Item "./bin/chim.exe" $install_dir -Force
