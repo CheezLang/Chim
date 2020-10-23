4a43e9e88911ed1ace5a489b68fd02f394e3a3c4
Nimaoth <nicolas.otremba@web.de>
2020-10-23 15:19:50 +0200

    refactored stuff, added icon

--- build.ps1
+++ build.ps1
@@ -2,4 +2,4 @@
 param ([Parameter(ValueFromRemainingArguments)] [string[]] $Passthrough)
 Copy-Item "../libraries/GLFW/glfw/lib/glfw3.dll" "./bin" -Force
 Copy-Item "./fonts" "./bin" -Recurse -Force
-&cheezc ./src/main.che --trace-stack --out ./bin --name chim --time --error-source --modules ../libraries/GLFW/glfw ../libraries/imgui/src --subsystem console --print-ast-analysed ./bin/int/ast.chea @Passthrough
+&cheezc ./src/main.che --trace-stack --out ./bin --name chim --time --error-source --modules ../libraries/GLFW/glfw ../libraries/imgui/src --subsystem console --print-ast-analysed ./bin/int/ast.chea --print-linker-args @Passthrough
