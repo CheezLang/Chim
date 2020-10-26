37d8f530c7bd09623037b1bc2562e24f3588f95b
Nimaoth <nicolas.otremba@web.de>
2020-10-27 00:47:44 +0100

    basic mapping of keys in lua

--- config/config.lua
+++ config/config.lua
@@ -1,1 +1,11 @@
+for x = 0, 10 do
+    chim.command("edit.insert", "uiae\n")
+end
 print("lua config")
+
+chim.map("normal", "w", "edit.insert", "word\n")
+chim.map("normal", "x", "buffer.delete_right")
+chim.map("normal", "u", "buffer.undo")
+chim.map("normal", "U", "buffer.redo")
+-- chim.map("command", "f{mov}", function(mov) print(mov) end)
+-- chim.map("command", "f{mov}", function(mov) print(mov) end)
